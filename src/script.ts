5fb5f158e93c83f776e1f9d510a9ae55078c3761
iss <ismailsaid.sari@stu.fsm.edu.tr>
2023-01-01 16:18:20 +0300

    dom event

--- src/script.ts
+++ src/script.ts
@@ -5,13 +5,10 @@ var shadow: boolean = true;
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
         shadow = false;
 
-document.addEventListener('DOMContentLoaded', () => {
+navigator.serviceWorker.register('./dist/sw.js');
 
-        navigator.serviceWorker.register('./dist/sw.js');
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-        const game = new Game(canvas, context);
-        game.shadow = shadow;
-});
+const game = new Game(canvas, context);
+game.shadow = shadow;
